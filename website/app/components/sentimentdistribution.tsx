8b95e1c712f0b6a16c57925aa4e7b1c6e5c5a581
Bruno Ceccolini <t-bceccolini@expediagroup.com>
2024-03-20 19:11:54 +0000

    update sentiment distribution

--- website/app/components/sentimentdistribution.tsx
+++ website/app/components/sentimentdistribution.tsx
@@ -11,7 +11,7 @@ import { Bar } from 'react-chartjs-2';
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
 
 const options = {
-  indexAxis: 'y' as const,
+  indexAxis: 'x' as const,
   layout: {
     autoPadding: true,
   },
@@ -22,22 +22,17 @@ const options = {
   },
   responsive: true,
   scales: {
-    x: {
-      ticks: {
-        maxRotation: 50,
-        minRotation: 30,
-        padding: 10,
-        autoSkip: false,
-        fontSize: 10,
-      },
-    },
     y: {
       ticks: {
-        maxRotation: 50,
-        minRotation: 0,
         padding: 5,
         autoSkip: false,
-        fontSize: 4,
+        max: 100,
+      },
+    },
+    x: {
+      ticks: {
+        padding: 10,
+        autoSkip: false,
       },
     },
   },
@@ -52,12 +47,21 @@ const SentimentDistribution = ({
   negative: number;
   title?: string;
 }) => {
+  // absolute value of negative
+  const absNegative = Math.abs(negative);
+  // total number of responses
+  const total = positive + absNegative;
+  // percentage of positive responses
+  const percentagePositive = (positive / total) * 100;
+  // percentage of negative responses
+  const percentageNegative = (absNegative / total) * 100;
+
   const data = {
     labels: ['Positive', 'Negative'],
     datasets: [
       {
         label: 'Sentiment Distribution',
-        data: [positive, negative],
+        data: [percentagePositive, percentageNegative],
         backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)'],
         borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)'],
         borderWidth: 1,
@@ -67,7 +71,7 @@ const SentimentDistribution = ({
   return (
     <div className="my-8 flex flex-col items-center justify-center">
       <h2 className="text-center">{title}</h2>
-      <div className="flex items-center flex-wrap w-full h-[300px] sm:w-[50vw] sm:h-[35vw] justify-center gap-4 mt-8 bg-white shadow-lg rounded-md p-4">
+      <div className="flex items-center flex-wrap h-[400px] w-full justify-center gap-4 mt-8 bg-white shadow-lg rounded-md p-4">
         <Bar data={data} options={options} />
       </div>
     </div>
